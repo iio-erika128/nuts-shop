57a14af092e9502ae62de4d51316734c93209655
iio-erika128 <e_sato128@yahoo.co.jp>
2022-03-08 09:32:25 +0900

    管理機能追加

--- index/purchase-output.php
+++ index/purchase-output.php
@@ -1,23 +1,57 @@
 <?php session_start(); ?>
-<?php require '../header.php'; ?>
+<?php require 'header.php'; ?>
 <?php require 'connect.php'; ?>
 <?php require 'menu.php'; ?>
 <?php
-//$pdo=new PDO('mysql:host=localhost;dbname=shop;charset=utf8', 'staff', 'password');
 $purchase_id=1;
+// A_I と同じことをやってる
 foreach ($pdo->query('select max(id) from purchase') as $row) {
-	$purchase_id=$row['max(id)']+1;
+	$purchase_id = $row['max(id)']+1;
 }
-$sql=$pdo->prepare('insert into purchase values(?,?)');
-if ($sql->execute([$purchase_id, $_SESSION['customer']['id']])) {
-	foreach ($_SESSION['product'] as $product_id=>$product) {
-		$sql=$pdo->prepare('insert into purchase_detail values(?,?,?)');
-		$sql->execute([$purchase_id, $product_id, $product['count']]);
-	}
-	unset($_SESSION['product']);
-	echo '購入手続きが完了しました。ありがとうございます。';
-} else {
+
+try{
+	//トランザクションの開始
+	$pdo->beginTransaction();
+
+	$sql=$pdo->prepare(
+		'INSERT into purchase(id, customer_id ) values(?,?)'
+	);							// このフィールド名が必要↑
+
+	$success = $sql->execute([
+				$purchase_id, 							// 1
+				$_SESSION['customer']['id'] // 2 
+		]);
+
+	if ($success) {
+//$purchase_id = $sql->lastInsertId(); A_Iで登録されたidの取得
+
+/*
+	一行実行のINSERT文は非効率なので,一回のINSERTで複数行の注文を入れたい
+	valuesの後ろの ()をカンマ区切りでつなげて作るが,結構難しいので今はやりません
+*/
+		foreach ($_SESSION['product'] as $product_id=>$product) {
+			$sql=$pdo->prepare(
+				'INSERT into purchase_detail values(?,?,?)'
+			);
+			$sql->execute([
+				$purchase_id, 
+				$product_id, 
+				$product['count']]
+			);
+		}
+
+		$pdo->commit();
+		// カートをカラにする
+		unset($_SESSION['product']);
+		echo '購入手続きが完了しました。ありがとうございます。';
+
+	} //if END, else はいらない
+
+} catch(PDOException $e) {
+  echo $e->getMessage();  // (8) エラーメッセージを出力
 	echo '購入手続き中にエラーが発生しました。申し訳ございません。';
-}
+  $pdo->rollBack();  // (9) ロールバック
+
+} 
 ?>
-<?php require '../footer.php'; ?>
+<?php require 'footer.php'; ?>
